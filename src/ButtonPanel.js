7821caeeecc0688b02853d20940a5767d02bdc7d
masontrang <mt9260@gmail.com>
2022-04-28 16:35:39 -0400

    Co-authored-by: gmay925 <gmay925@users.noreply.github.com> Co-authored-by: heyadrienne <heyadrienne@users.noreply.github.com> Co-authored-by: wreed3 <wreed3@users.noreply.github.com> Co-authored-by: masterlynx2 <masterlynx2@users.noreply.github.com> Co-authored-by: antonioWatkins <antonioWatkins@users.noreply.github.com>

--- src/ButtonPanel.js
+++ src/ButtonPanel.js
@@ -1,26 +1,25 @@
-import Button from './Button'
+import Button from "./Button";
 
-function ButtonPanel(){
-
-    return(
-        <div className='component-button-panel'> 
-        <div>
-            <Button value={7}/>
-            <Button value={8}/>
-            <Button value={9}/>
-        </div>
-        <div>
-            <Button value={4}/>
-            <Button value={5}/>
-            <Button value={6}/>
-        </div>
-        <div>
-            <Button value={1}/>
-            <Button value={2}/>
-            <Button value={3}/>
-        </div>
-        </div>
-    )
+function ButtonPanel() {
+  return (
+    <div className="component-button-panel">
+      <div>
+        <Button value={7} />
+        <Button value={8} />
+        <Button value={9} />
+      </div>
+      <div>
+        <Button value={4} />
+        <Button value={5} />
+        <Button value={6} />
+      </div>
+      <div>
+        <Button value={1} />
+        <Button value={2} />
+        <Button value={3} />
+      </div>
+    </div>
+  );
 }
 
-export default ButtonPanel
+export default ButtonPanel;
